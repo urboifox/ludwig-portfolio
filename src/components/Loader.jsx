ad6e830bfe4d659dfd2c22de49158c42f9e066f9
gunblade <mohamed.dev.egy@gmail.com>
2023-05-30 01:12:23 +0200

    styled loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,9 @@
 function Loader() {
-  return <div>Loader</div>;
+  return (
+    <div className="font-montserrat bg-background text-[30px] w-full h-[100vh] flex items-center justify-center">
+      <h1 className="text-primary">Loading...</h1>
+    </div>
+  );
 }
 
 export default Loader;
